0e055a6d08fffa14590c1002e3f991d0c8a6f5b8
Hamza BEDOUI <hamza.bedoui@ausy.fr>
2022-10-05 14:47:08 +0200

    create PurchaseMessage Notification

--- src/Controller/StockTransactionController.php
+++ src/Controller/StockTransactionController.php
@@ -16,24 +16,25 @@ class StockTransactionController extends AbstractController
     public function buy(MessageBusInterface $bus): Response
     {
         // $notification->getOrder()->getBuyer()->getEmail()
-        //$order = new class {
-        //    public function getId()
-        //    {
-        //        return 1;
-        //    }
-        //    public function getBuyer(): object
-        //    {
-        //        return new class {
-        //            public function getEmail(): string
-        //            {
-        //                return 'email@example.tech';
-        //            }
-        //        };
-        //    }
-        //};
+        $order = new class {
+           public function getId()
+           {
+               return 1;
+           }
+           public function getBuyer(): object
+           {
+               return new class {
+                   public function getEmail(): string
+                   {
+                       return 'email@example.tech';
+                   }
+               };
+           }
+        };
 
 
         // 1. Dispatch confirmation message
+         //$bus->dispatch(new PurchaseConfirmationNotification($order));
         $bus->dispatch(new SaveOrder());
 
         // 2. Display confirmation to the user
---
+++ src/Message/PurchaseConfirmationNotification.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Message;
+
+class PurchaseConfirmationNotification
+{
+    public function __construct(private object $order)
+    {
+
+    }
+
+    public function getOrder(): object
+    {
+        return $this->order;
+
+    }
+
+}
+
